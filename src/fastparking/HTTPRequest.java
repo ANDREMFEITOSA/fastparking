384665fe2922a02d889fc748e5c8d4c88d291640
Jhoylan Goncalves <jhoylangs@gmail.com>
2021-01-28 14:53:37 -0300

    researching the closest garage

---
+++ src/fastparking/HTTPRequest.java
@@ -0,0 +1,121 @@
+package fastparking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class HTTPRequest {
+	//Method1: java.net.HttpURLConnection
+	private static HttpURLConnection connection;
+	private String key = "AIzaSyDqGIMchOeXPAlfe1qzQjAlUt5QVf6lh8s";
+	private BufferedReader reader;
+	private String line;
+	private StringBuffer responseContent = new StringBuffer();
+	private List<String> distances = new ArrayList<>();
+	private int lines = 0;
+	
+	public void request(String origin, String destination) {
+		try {
+			URL url = new URL("https://maps.googleapis.com/maps/api/distancematrix/json?"
+					+ "units=si&origins=" + origin + "&destinations=" + destination + "&key=" + key);
+			connection = (HttpURLConnection) url.openConnection();
+			
+			//Request method
+			connection.setRequestMethod("GET");
+			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+			
+			int status = connection.getResponseCode();
+			
+			if(status > 299) {
+				reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+				
+				while((line = reader.readLine()) != null) {
+					responseContent.append(line);
+				}
+				
+				reader.close();
+			}else {
+				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				
+				while((line = reader.readLine()) != null) {
+					if (line.contains("km")) {
+						distances.add(line);
+					}
+				}
+				
+				reader.close();
+			}
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			connection.disconnect();
+		}
+		
+	}	
+
+	public List<Float> distancesValues() {
+		List<Float> floatDistances = new ArrayList<>();
+		
+		for(int i =0; i < this.distances.size(); i++) {
+			String distanceString = distances.get(i).substring(28, distances.get(i).length() - 5);
+			distances.set(i, distanceString);
+			float distance = Float.parseFloat(distanceString);
+			floatDistances.add(distance);
+		}
+		
+		return floatDistances;
+	}
+
+	public int getTheClosestGaragePosition(List<Float> garagesDistances) {
+		int closestGaragePosition = 0;
+		float min = garagesDistances.get(0);
+		
+		for(int i = 1; i < garagesDistances.size(); i++) {
+			if(garagesDistances.get(i) < min) {
+				min = garagesDistances.get(i);
+				closestGaragePosition = i;
+			}
+		}
+		
+		return closestGaragePosition;
+	}
+
+	public String formattingDestinationString(List<Garage> garages) {
+		String destination = "";
+		
+		for(int i = 0; i < garages.size(); i++) {
+			destination += garages.get(i).getLocation();
+			
+			if(i < garages.size() - 1) {
+				destination += "|";
+			}
+		}
+		
+		return destination;
+	}
+
+	public Garage closestGarage(List<Garage> garages, Driver driver) {
+		String origin = driver.getLocation();
+		String destination = this.formattingDestinationString(garages);
+		
+		this.request(origin, destination);
+		
+		int position = this.getTheClosestGaragePosition(this.distancesValues());
+		
+		return garages.get(position);
+	}
+}
---
+++ src/fastparking/HTTPRequestTest.java
@@ -0,0 +1,57 @@
+package fastparking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class HTTPRequestTest {
+
+	@Test
+	void requestTest() {
+		HTTPRequest test = new HTTPRequest();
+		String house = "-1.3688387,-48.4719525|-1.4110308,-48.4732266";
+		String uata = "-1.4313741,-48.4647346";
+		
+		//test.request(uata, house);
+	}
+	
+	@Test
+	void integerDistancesValuesTest() {
+		HTTPRequest test = new HTTPRequest();
+		String house = "-1.3688387,-48.4719525|-1.4110308,-48.4732266";
+		String uata = "-1.4313741,-48.4647346";
+		
+		test.request(uata, house);
+		test.distancesValues();
+	}
+	
+	@Test
+	void getTheClosestGarage() {
+		HTTPRequest test = new HTTPRequest();
+		BigDecimal PRICE = new BigDecimal("10");
+		
+		Garage home = new Garage("-1.3688387,-48.4719525", PRICE, "home");
+		Garage momsHome = new Garage("-1.4110308,-48.4732266", PRICE, "momshome");
+		Garage uata = new Garage("-1.4313741,-48.4647346", PRICE, "uata");
+		
+		List<Garage> garages = new ArrayList<>();
+		garages.add(home);
+		garages.add(momsHome);
+		garages.add(uata);
+		
+		String casota = "-1.4078921,-48.4658382";
+		
+		Driver jhow = new Driver();
+		jhow.setLocation(casota);
+		
+		Garage closest = test.closestGarage(garages, jhow);
+		
+		System.out.println(closest.getName());
+	}
+	
+
+}
