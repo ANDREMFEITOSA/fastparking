4c8e5af5c1ccf4b5bbb336a1490dbe8b7b1c2309
andre feitosa <andremfeitosa@hotmail.com>
2021-02-05 17:43:11 -0300

    implementing the PaymentTest class

--- src/fastparking/PaymentTest.java
+++ src/fastparking/PaymentTest.java
@@ -1,6 +1,7 @@
 package fastparking;
 
 import java.math.BigDecimal;
+import java.text.NumberFormat;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,16 +11,20 @@ class PaymentTest {
 	@Test
 	void hostPaymentAmountTest() {
 		
+		Routes routes = new Routes();
+		GarageDatabase garageDatabase = new GarageDatabase(routes);
 		Garage garage = new Garage("Antônio Barreto", "Uata");
-		Driver driver = new Driver();
+		Driver driver = new Driver(garageDatabase);
 		Host host = new Host("João", "000.000.000-00", "xxx", "000000", garage, 10);
 		
 		BigDecimal serviceFee = BigDecimal.valueOf(0.05);
-		BigDecimal rentalPrice = BigDecimal.valueOf(5);
+		
 		Payment payment = new Payment(host, driver, serviceFee);
 		
-		Assertions.assertEquals("R$ 10,00", payment.driverPaymentAmount(rentalPrice, 2));
-		Assertions.assertEquals("R$ 9,50", payment.hostPaymentAmount(rentalPrice, 2));
+		String driverValue = NumberFormat.getCurrencyInstance().format(payment.driverPaymentAmount(BigDecimal.valueOf(2.5)));
+		String hostValue = NumberFormat.getCurrencyInstance().format(payment.hostPaymentAmount(BigDecimal.valueOf(2.5)));
+		
+		Assertions.assertEquals("R$ 12,50", driverValue);
+		Assertions.assertEquals("R$ 11,88", hostValue);
 	}
-
 }
