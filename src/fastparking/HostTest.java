30f8b9848d8425feb75e9f32db15289f0bdf9b17
andre feitosa <andremfeitosa@hotmail.com>
2021-02-05 17:39:44 -0300

    implementing the HostTest class

---
+++ src/fastparking/HostTest.java
@@ -0,0 +1,95 @@
+package fastparking;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HostTest {
+
+	@Test
+	void enableGarageTest() {
+		
+		Routes route = new Routes();
+		
+		GarageDatabase garageDatabase = new GarageDatabase(route);
+		
+		Garage garage_joao = new Garage("Antônio Barreto", "Uata");
+		Garage garage_paulo = new Garage("Boa Ventura", "Vikings");
+		Garage garage_andre = new Garage("Oliveira Belo", "El Patron");
+		
+		Host host_joao = new Host("João", "111.000.000-01", "xxx", "000000", garage_joao, 10);
+		Host host_paulo = new Host("Paulo", "222.000.000-02", "yyy", "000000", garage_paulo, 10);
+		Host host_andre = new Host("Andre", "333.000.000-03", "zzz", "000000", garage_paulo, 10);
+		
+		host_joao.enableGarage(garageDatabase);
+		host_paulo.enableGarage(garageDatabase);
+		host_andre.enableGarage(garageDatabase);
+		
+		Assertions.assertEquals(3, garageDatabase.getNumberOfAvailableGarages());
+	}
+	
+	@Test
+	void disableGarageTest() {
+		Routes route = new Routes();
+		
+		GarageDatabase garageDatabase = new GarageDatabase(route);
+		
+		Garage garage_joao = new Garage("Antônio Barreto", "Uata");
+		Garage garage_paulo = new Garage("Boa Ventura", "Vikings");
+		Garage garage_andre = new Garage("Oliveira Belo", "El Patron");
+		
+		Host host_joao = new Host("João", "111.000.000-01", "xxx", "000000", garage_joao, 10);
+		Host host_paulo = new Host("Paulo", "222.000.000-02", "yyy", "000000", garage_paulo, 10);
+		Host host_andre = new Host("Andre", "333.000.000-03", "zzz", "000000", garage_paulo, 10);
+		
+		host_joao.enableGarage(garageDatabase);
+		host_paulo.enableGarage(garageDatabase);
+		host_andre.enableGarage(garageDatabase);
+		
+		host_joao.disableGarage(garageDatabase);
+		
+		Assertions.assertEquals(2, garageDatabase.getNumberOfAvailableGarages());
+	}
+	
+	@Test
+	void confirmCheckInTest() {
+		Routes route = new Routes();
+		
+		GarageDatabase garageDatabase = new GarageDatabase(route);
+		
+		Garage garage_joao = new Garage("Antônio Barreto", "Uata");
+		Garage garage_paulo = new Garage("Boa Ventura", "Vikings");
+		Garage garage_andre = new Garage("Oliveira Belo", "El Patron");
+		
+		Host host_joao = new Host("João", "111.000.000-01", "xxx", "000000", garage_joao, 10);
+		Host host_paulo = new Host("Paulo", "222.000.000-02", "yyy", "000000", garage_paulo, 10);
+		Host host_andre = new Host("Andre", "333.000.000-03", "zzz", "000000", garage_paulo, 10);
+		
+		host_joao.enableGarage(garageDatabase);
+		host_paulo.enableGarage(garageDatabase);
+		host_andre.enableGarage(garageDatabase);
+		
+		Driver driver_pedro = new Driver(garageDatabase);
+		host_joao.getGarage().setTimeCheckIn(driver_pedro);
+		
+		Driver driver_tiago = new Driver(garageDatabase);
+		host_paulo.getGarage().setTimeCheckIn(driver_tiago);
+		
+		host_joao.confirmCheckIn(garageDatabase);
+		host_paulo.confirmCheckIn(garageDatabase);
+		
+		LocalDateTime joao_timeCheckIn = host_joao.getGarage().getTimeCheckIn();
+		LocalDateTime paulo_timeCheckIn = host_paulo.getGarage().getTimeCheckIn();
+		Duration duration = Duration.between(paulo_timeCheckIn, joao_timeCheckIn);
+		
+		BigDecimal durationTest = BigDecimal.valueOf(duration.toMinutes());
+		
+		Assertions.assertEquals(1, garageDatabase.getNumberOfAvailableGarages());
+		
+		Assertions.assertEquals("0", durationTest.toPlainString());
+	}
+}
