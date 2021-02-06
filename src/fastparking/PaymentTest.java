48a4030d567d164eb238b73ef3905c4222ce865e
andre feitosa <andremfeitosa@hotmail.com>
2021-02-06 19:18:33 -0300

    changing constructors in the tests

--- src/fastparking/PaymentTest.java
+++ src/fastparking/PaymentTest.java
@@ -13,16 +13,16 @@ class PaymentTest {
 		
 		Routes routes = new Routes();
 		GarageDatabase garageDatabase = new GarageDatabase(routes);
-		Garage garage = new Garage("Antônio Barreto", "Uata");
-		Driver driver = new Driver(garageDatabase);
+		Garage garage = new Garage("Antônio Barreto", "Uata", new BigDecimal("1000"), new BigDecimal("1000"));
+		Driver driver = new Driver(garageDatabase, new Car("JTDZN3EU0E3298500"));
 		Host host = new Host("João", "000.000.000-00", "xxx", "000000", garage, 10);
 		
 		BigDecimal serviceFee = BigDecimal.valueOf(0.05);
 		
-		Payment payment = new Payment(host, driver, serviceFee);
+		PaymentCalculation payment = new PaymentCalculation(garage);
 		
-		String driverValue = NumberFormat.getCurrencyInstance().format(payment.driverPaymentAmount(BigDecimal.valueOf(2.5)));
-		String hostValue = NumberFormat.getCurrencyInstance().format(payment.hostPaymentAmount(BigDecimal.valueOf(2.5)));
+		String driverValue = NumberFormat.getCurrencyInstance().format(payment.driverPaymentAmount());
+		String hostValue = NumberFormat.getCurrencyInstance().format(payment.hostPaymentAmount());
 		
 		Assertions.assertEquals("R$ 12,50", driverValue);
 		Assertions.assertEquals("R$ 11,88", hostValue);
