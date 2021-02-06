8889a1b7d18fff467d1771cfc603cd4a72a90a14
Jhoylan Goncalves <jhoylangs@gmail.com>
2021-02-05 21:50:45 -0300

    fixing the possibleGarages method

--- src/fastparking/GarageDatabase.java
+++ src/fastparking/GarageDatabase.java
@@ -8,52 +8,52 @@ public class GarageDatabase {
 	GarageSearch search;
 	private List<Garage> availableGarages;
 	private Routes route;
-			
+
 	public GarageDatabase(Routes routes) {
 		this.search = new GarageSearch();
 		this.availableGarages = new ArrayList<>();
 		this.route = routes;
 	}
-	
+
 	public void enableGarage(Garage garage) {
 		this.availableGarages.add(garage);
 	}
-	
+
 	public void disableGarage(Garage garage) {
 		this.availableGarages.remove(garage);
 	}
-	
+
 	public Garage searchTheClosestGarage(Driver driver) {
-		if(this.possibleGarages(driver).size() > 0) {
+		if (this.possibleGarages(driver).size() > 0) {
 			return search.closestGarage(this.possibleGarages(driver), driver);
-		}else {
+		} else {
 			return null;
-		}		
+		}
 	}
-	
+
 	public int getNumberOfAvailableGarages() {
 		return availableGarages.size();
 	}
-	
+
 	public void showRoute(String origin, String destination) {
 		int routeNumber = this.route.getRouteNumber();
 		this.route.calculatingRoute(origin, destination);
 		this.search.showRoute(routeNumber);
 	}
-	
-	public List<Garage> possibleGarages(Driver driver){
+
+	public List<Garage> possibleGarages(Driver driver) {
 		List<Garage> possibleGarages = new ArrayList<>();
+
+		LocalDateTime driverActualTime = LocalDateTime.now();
+		LocalDateTime driverDepartureTime = driverActualTime
+				.plusSeconds(driver.getReservationTime());
 		
 		for(int i = 0; i < availableGarages.size(); i++) {
-			LocalDateTime driverActualTime = LocalDateTime.now();
-			LocalDateTime driverDepartureTime = driverActualTime.plusSeconds
-					(driver.getReservationTime());
-			
-			if(driverDepartureTime.isBefore(availableGarages.get(i).getCloseTime())) {
+			if (driverDepartureTime.isBefore(availableGarages.get(i).getCloseTime())) {
 				possibleGarages.add(availableGarages.get(i));
 			}
-		}
-		
+		}		
+			
 		return possibleGarages;
 	}
 }
