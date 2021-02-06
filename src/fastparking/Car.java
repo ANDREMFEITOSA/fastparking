6fda1422511b8bcf2ac0363048be2ab60fd9b2e3
Jhoylan Goncalves <jhoylangs@gmail.com>
2021-02-06 16:07:40 -0300

    setting car dimensions

--- src/fastparking/Car.java
+++ src/fastparking/Car.java
@@ -1,10 +1,19 @@
 package fastparking;
 
+import java.math.BigDecimal;
+
 public class Car {
-	
 	private String licensePlate;
 	private String document;
 	private String model;
-	private float[] dimensions; // vetor 3 itens: comprimento, largura, altura	
+	private BigDecimal width;
+	private BigDecimal length;
 	
+	public void setCarDimensions(String vin) {
+		CarDimensionsResearch specifications = new CarDimensionsResearch();
+		specifications.request(vin);
+		
+		width = specifications.getWidth();
+		length = specifications.getLength();
+	}
 }
