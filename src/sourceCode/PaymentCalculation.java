679f9573099ec3da2d1fed132b946b1cba024ef5
Jhoylan Goncalves <jhoylangs@gmail.com>
2021-02-07 20:12:49 -0300

    driver fee calculation for reservation time run out

--- src/sourceCode/PaymentCalculation.java
+++ src/sourceCode/PaymentCalculation.java
@@ -10,13 +10,15 @@ public class PaymentCalculation {
 	private BigDecimal rentalPrice;
 	
 	private final BigDecimal serviceFee = new BigDecimal("0.15");
+	
+	private Garage garage;
 
 	public PaymentCalculation(Garage garage) {
-	
-		this.rentalPrice = garage.getPrice().multiply(garage.getDuration());
+		this.garage = garage;
 	}
 	
 	public BigDecimal hostPaymentAmount() {
+		this.rentalPrice = garage.getPrice().multiply(garage.getDuration());
 		
 		BigDecimal amount = this.rentalPrice.multiply(BigDecimal.ONE.subtract(serviceFee));
 		
