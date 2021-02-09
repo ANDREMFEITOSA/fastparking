30c03f1a049c2d649ce363a526e58c0d6708b450
Jhoylan Goncalves <jhoylangs@gmail.com>
2021-02-09 10:21:17 -0300

    fixing the cancelation logic

--- src/sourceCode/Reservation.java
+++ src/sourceCode/Reservation.java
@@ -1,6 +1,7 @@
 package sourceCode;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -9,17 +10,27 @@ public class Reservation {
 	private Timer timer;
 	private Garage garage;
 	private Database database;
+	private LocalDateTime reservationEndTime;
 	
 	public Reservation(int seconds, Garage garage, Database database, Driver driver) {
+		this.reservationEndTime = LocalDateTime.now().plusSeconds(seconds);
+		this.database = database;
 		this.garage = garage;
 		this.timer = new Timer();
 		this.timer.schedule(new Verification(garage, database, driver), seconds * 1000);
 	}
 
 	public void cancelReservation() {
-		System.out.println("You've cancel the reservation");
-		this.database.garageDatabase.enableGarage(this.garage, this);
-		timer.cancel();
+		LocalDateTime callMoment = LocalDateTime.now();
+		
+		if(callMoment.isBefore(this.reservationEndTime)) {
+			System.out.println("You've cancel the reservation");
+			this.database.garageDatabase.enableGarage(this.garage, this);
+			timer.cancel();
+		}else {
+			System.out.println("Sorry , its too late to cancel your reservation");
+		}
+		
 	}
 
 	class Verification extends TimerTask {
@@ -34,9 +45,7 @@ public class Reservation {
 		}
 
 		public void run() {
-			if (this.garage.driverIsInTheGarage()) {
-				System.out.println("Drive is in the garage");
-			} else {
+			if (!this.garage.driverIsInTheGarage()) {
 				if (this.garage.isOpen()) {
 					this.database.garageDatabase.enableGarage(this.garage, this);
 				}
@@ -45,8 +54,7 @@ public class Reservation {
 				
 				System.out.println(this.driver.getName() + ", Your reservation time run out! " 
 						+ "You're gonna be charged: R$ "
-						+ this.garage.getPrice().divide(new BigDecimal("2")));				
-				
+						+ this.garage.getPrice().divide(new BigDecimal("2")));	
 			}
 			
 			RoutesManangement.deleteRoute(this.driver.getActualRouteNumber());
