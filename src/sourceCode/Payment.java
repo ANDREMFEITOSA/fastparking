2dded29813158fb0c274edf4aa53e2072866bd81
Jhoylan Goncalves <jhoylangs@gmail.com>
2021-02-09 08:39:16 -0300

    improve Payment redability

--- src/sourceCode/Payment.java
+++ src/sourceCode/Payment.java
@@ -14,20 +14,19 @@ public class Payment {
 	public Payment(Object obj){
 		if(obj instanceof Driver) {
 			PaymentCalculation paymentCalculation = new PaymentCalculation(((Driver) obj).getGarage());
+			this.driver = (Driver) obj;
 			this.valueDriver = paymentCalculation.driverPaymentAmount().
-					subtract(((Driver) obj).getCredit());
+					subtract(this.driver.getCredit());
 			this.valueHost = paymentCalculation.hostPaymentAmount();
-			this.host = ((Driver) obj).getGarage().getHost();
-			this.driver = ((Driver) obj);
-		}else if(obj instanceof Verification){
+			this.host = this.driver.getGarage().getHost();
+			}else if(obj instanceof Verification){
 			PaymentCalculation paymentCalculation = new PaymentCalculation(((Verification) obj).getDriver().getGarage());
 			
 			this.driver = ((Verification) obj).getDriver();
-			this.host = ((Verification) obj).getDriver().getGarage().getHost();
-			this.valueDriver = ((Verification) obj).getDriver().getGarage()
-					.getPrice().divide(new BigDecimal("2"));
-			this.valueHost = this.valueDriver
-					.multiply(BigDecimal.ONE.subtract(paymentCalculation.getServiceFee()));
+			this.host = this.driver.getGarage().getHost();
+			this.valueDriver = this.driver.getGarage().getPrice().divide(new BigDecimal("2"));
+			this.valueHost = this.valueDriver.multiply(BigDecimal.ONE.
+					subtract(paymentCalculation.getServiceFee()));
 		}
 	}
 	
