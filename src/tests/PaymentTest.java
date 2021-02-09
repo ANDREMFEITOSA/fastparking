7e6487c828451289ec684504b39aff5d689e0373
andre feitosa <andremfeitosa@hotmail.com>
2021-02-09 00:15:18 -0300

    updating the PaymentTest class

--- src/tests/PaymentTest.java
+++ src/tests/PaymentTest.java
@@ -8,25 +8,75 @@ import org.junit.jupiter.api.Test;
 
 import sourceCode.*;
 class PaymentTest {
+	
+	@Test
+	void paymentAmountWithCheckInTest() throws InterruptedException {
+
+		Database database = new Database();
+		
+		Garage garage_joao = new Garage("-1.4313795,-48.4625459", "Uata", new BigDecimal("1000"), new BigDecimal("1000"));
+		
+		garage_joao.setCloseTime(30);
+				
+		Host host_joao = new Host("João", "111.000.000-01", "xxx", "", garage_joao, database);
+		
+		database.hostsDatabase.add(host_joao);
+		
+		host_joao.enableGarage();
+		
+		Driver driver_pedro = new Driver(new Car("JTDZN3EU0E3298500"), database, "Pedro");
+		
+		driver_pedro.setLocation("-1.3688387,-48.4719525");
+		
+		driver_pedro.reserveGarage();
+		
+		driver_pedro.confirmCheckIn();
+		
+		host_joao.confirmCheckIn();
+		
+		int seconds = 10;
+		Thread.sleep(seconds * 1000);
+		
+		host_joao.confirmCheckOut(5);
+		
+		driver_pedro.ConfirmCheckOut(5);
+		
+		driver_pedro.pay();
+		
+		BigDecimal driverValue = database.paymentDatabase.getPayment(0).getValueDriver();
+		BigDecimal hostValue = database.paymentDatabase.getPayment(0).getValueHost();
+		
+		Assertions.assertEquals(new BigDecimal("50"), driverValue);
+		Assertions.assertEquals(new BigDecimal("42.50"), hostValue);
+	}
 
 	@Test
-	void hostPaymentAmountTest() {
+	void paymentAmountWithoutCheckInTest() throws InterruptedException {
+
+		Database database = new Database();
 		
-		RouteCalculation routes = new RouteCalculation();
-		GarageDatabase garageDatabase = new GarageDatabase(routes);
-		Garage garage = new Garage("Antônio Barreto", "Uata", new BigDecimal("1000"), new BigDecimal("1000"));
-		PaymentDatabase paymentDatabase = new PaymentDatabase();
-		Driver driver = new Driver(new Car("JTDZN3EU0E3298500"), null, null);
-		Host host = new Host("João", "000.000.000-00", "xxx", "", garage, null);
+		Garage garage_joao = new Garage("-1.4313795,-48.4625459", "Uata", new BigDecimal("1000"), new BigDecimal("1000"));
 		
-		BigDecimal serviceFee = BigDecimal.valueOf(0.05);
+		garage_joao.setCloseTime(30);
+				
+		Host host_joao = new Host("João", "111.000.000-01", "xxx", "", garage_joao, database);
 		
-		PaymentCalculation payment = new PaymentCalculation(garage);
+		database.hostsDatabase.add(host_joao);
 		
-		String driverValue = NumberFormat.getCurrencyInstance().format(payment.driverPaymentAmount());
-		String hostValue = NumberFormat.getCurrencyInstance().format(payment.hostPaymentAmount());
+		host_joao.enableGarage();
 		
-		Assertions.assertEquals("R$ 12,50", driverValue);
-		Assertions.assertEquals("R$ 11,88", hostValue);
+		Driver driver_pedro = new Driver(new Car("JTDZN3EU0E3298500"), database, "Pedro");
+		
+		driver_pedro.setLocation("-1.3688387,-48.4719525");
+		
+		driver_pedro.reserveGarage();
+		
+		Thread.sleep(16 * 1000);
+		
+		BigDecimal driverValue = database.paymentDatabase.getPayment(0).getValueDriver();
+		BigDecimal hostValue = database.paymentDatabase.getPayment(0).getValueHost();
+		
+		Assertions.assertEquals(new BigDecimal("2.5"), driverValue);
+		Assertions.assertEquals(new BigDecimal("2.125"), hostValue);
 	}
 }
